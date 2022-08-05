d566c6654a899d9c06d553ca333bae395978fb57
Rodrigo Pombo <pombopombopombo@gmail.com>
2022-08-05 09:19:35 +0000

    Move to scrollycoding to posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/content/posts/`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-mdx`,
       options: {
         mdxOptions: {
